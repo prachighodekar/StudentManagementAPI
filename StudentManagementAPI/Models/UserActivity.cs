b86c00359d99a950ba7766eb770cfb5442447f62
prach <prach@PRASHAN>
2024-12-10 15:59:02 -0500

    Add project files.

---
+++ StudentManagementAPI/Models/UserActivity.cs
@@ -0,0 +1,9 @@
+﻿namespace StudentManagementAPI.Models
+{
+    public class UserActivity
+    {
+        public string CreatedBy { get; set; }
+        public DateTime CreatedOn { get; set; }
+
+    }
+}
